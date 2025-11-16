9f0e7895537d6fff392c05aee1009b26f03e30df
shreenat <shreenat@umd.edu>
2025-11-15 19:13:56 -0500

    basic API response

--- LingoLearn/src/App.tsx
+++ LingoLearn/src/App.tsx
@@ -1,13 +1,38 @@
-//import { useState } from 'react'
+import { useState } from 'react'
 import './App.css'
+import { GenerateContentResponse, GoogleGenAI } from "@google/genai";
+
 
 function App() {
   //const [count, setCount] = useState(0)
 
+  const [apiResponse, setApiResponse] = useState<string>("");
+  // The client gets the API key from the environment variable `GEMINI_API_KEY`.
+  const ai = new GoogleGenAI({
+    apiKey: import.meta.env.VITE_GEMINI_API_KEY
+  });
+
+  async function generateResponse() {
+    const response : GenerateContentResponse= await ai.models.generateContent({
+      model: "gemini-2.5-flash",
+      contents: "Explain how AI works in a few words",
+    });
+      const output = await response.text
+      if (output != undefined) {
+        setApiResponse(output);
+      } else {
+        console.log("No response from API");
+      }
+
+    
+    //console.log(response.text);
+  }
+  
   return (
     <>
       <h1>Lingo Learn!</h1>
-      
+      <button onClick={generateResponse}>Generate Response</button>
+      <p>{apiResponse}</p>
     </>
   )
 }
